f78e30b466680195a8d27eeeecbf8991a8417459
Kevin Gliewe <kevingliewe@outlook.de>
2018-08-22 23:41:28 +0200

    Add automatic versioning

---
+++ Goui/Properties/VersionInfoStatic.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Goui")]
+[assembly: AssemblyDescription("A small cross-platform UI library that brings the simplicity of native UI development to the web ")]
+[assembly: AssemblyCompany("Gliewe.NET")]
+[assembly: AssemblyProduct("Goui")]
+[assembly: AssemblyCopyright("Copyright © Kevin Gliewe")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
