d095626412c59427a46e1fba867f740c045c0b79
Kevin Gliewe <kevingliewe@gmail.com>
2019-03-27 16:01:02 +0100

    Cleanup for deploy |[PN]|

--- Goui.Forms/Renderers/EntryRenderer.cs
+++ Goui.Forms/Renderers/EntryRenderer.cs
@@ -178,9 +178,7 @@ namespace Goui.Forms.Renderers
 
         void UpdateText ()
         {
-            // ReSharper disable once RedundantCheckBeforeAssignment
-//            if (Control.Value != Element.Text)
-                Control.Value = Element.Text;
+            Control.Value = Element.Text;
         }
     }
 }
